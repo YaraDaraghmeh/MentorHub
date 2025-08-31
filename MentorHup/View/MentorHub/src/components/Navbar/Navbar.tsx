4654e3ca6d0a3fcb8965374e14bfc0ff9ddcf482
Sara-Sayed-Ahmad <sarasayedahmad24@gmail.com>
2025-08-31 21:36:02 +0300

    Add promary colors and Navbar component

---
+++ MentorHup/View/MentorHub/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+const Nav = () => {
+  return (
+    <nav className="relative bg-dark">
+      <div className="flex">
+        <a>{/* <img src="../MentorHub-logo (1)/cover.png"></img> */}</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
