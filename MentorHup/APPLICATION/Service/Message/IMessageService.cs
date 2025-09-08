da3c45825dcec28eaead574a8b2ef30a861d688e
khaled hroub <khaledmazen2002@gmail.com>
2025-09-08 04:19:32 +0300

    Add real-time communication using SignalR

---
+++ MentorHup/APPLICATION/Service/Message/IMessageService.cs
@@ -0,0 +1,13 @@
+﻿using MentorHup.APPLICATION.DTOs.DTOs;
+
+
+namespace MentorHup.APPLICATION.Service.Message
+{
+    public interface IMessageService
+    {
+        Task<MessageDto> SendMessageAsync(string senderId, CreateMessageDto dto);
+        Task<List<MessageDto>> GetConversationAsync(string userId, string otherUserId);
+
+        Task<MentorHup.Domain.Entities.Message?> MarkMessageAsReadAsync(int messageId);
+    }
+}
---
+++ MentorHup/Infrastructure/Hubs/ChatHub.cs
@@ -0,0 +1,41 @@
+﻿using MentorHup.APPLICATION.DTOs.DTOs;
+using MentorHup.APPLICATION.Service.Message;
+using Microsoft.AspNetCore.SignalR;
+using System.Security.Claims;
+
+public class ChatHub : Hub
+{
+    private readonly IMessageService _messageService;
+
+    public ChatHub(IMessageService messageService)
+    {
+        _messageService = messageService;
+    }
+
+    public async Task SendMessage(string receiverId, string content)
+    {
+        var senderId = Context.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        if (senderId == null) throw new Exception("UserIdentifier is null");
+
+
+        var dto = new CreateMessageDto { Content = content, ReceiverId = receiverId };
+
+        var message = await _messageService.SendMessageAsync(senderId, dto);
+
+        // إرسال الرسالة للطرف الآخر
+        await Clients.User(receiverId).SendAsync("ReceiveMessage", message);
+        await Clients.Caller.SendAsync("ReceiveMessage", message);
+    }
+
+    public async Task MarkMessageAsRead(int messageId)
+    {
+        var message = await _messageService.MarkMessageAsReadAsync(messageId);
+
+        if (message != null)
+        {
+            // إرسال حدث Real-Time للمرسل ليعرف أن الرسالة تم قراءتها
+            await Clients.User(message.SenderId).SendAsync("MessageRead", messageId);
+        }
+    }
+
+}
