1d8af9b6e59e3bdb35b0a9d4e843631e03818c66
Amjad-Hamidi <amjada871@yahoo.com>
2025-09-21 21:30:14 +0300

    apply Snapshot so Date is Now not UtcNow

--- MentorHup/Migrations/20250921170234_change_UtcNow_to_Now_of_CreatedAt_property.Designer.cs
+++ MentorHup/Migrations/20250921170234_change_UtcNow_to_Now_of_CreatedAt_property.Designer.cs
@@ -4,6 +4,7 @@ using MentorHup.Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MentorHup.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250921170234_change_UtcNow_to_Now_of_CreatedAt_property")]
+    partial class change_UtcNow_to_Now_of_CreatedAt_property
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
