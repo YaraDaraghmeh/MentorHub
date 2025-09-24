ba6b1eebf93904e4d86c68c752486e127c019eb0
Amjad-Hamidi <amjada871@yahoo.com>
2025-09-24 17:25:05 +0300

    restore Authorization on ChatHub

--- MentorHup/Infrastructure/Hubs/ChatHub.cs
+++ MentorHup/Infrastructure/Hubs/ChatHub.cs
@@ -1,7 +1,9 @@
 ﻿using MentorHup.APPLICATION.DTOs.DTOs;
 using MentorHup.APPLICATION.Service.Message;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 
+[Authorize]
 public class ChatHub : Hub
 {
     private readonly IMessageService _messageService;
