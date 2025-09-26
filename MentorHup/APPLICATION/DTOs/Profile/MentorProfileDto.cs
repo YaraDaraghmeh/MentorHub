5ef44fc3a76731a4a3a952223713932f25568716
Amjad-Hamidi <amjada871@yahoo.com>
2025-09-26 15:27:59 +0300

    add UserName property to MentorProfileDto & Name property to MenteeProfileDto

--- MentorHup/APPLICATION/DTOs/Profile/MentorProfileDto.cs
+++ MentorHup/APPLICATION/DTOs/Profile/MentorProfileDto.cs
@@ -7,6 +7,7 @@ namespace MentorHup.APPLICATION.DTOs.Profile
         public int Id { get; set; }
         public string ApplicationUserId { get; set; }
         public string Email { get; set; }
+        public string UserName { get; set; }
         public string Name { get; set; }
         public string CompanyName { get; set; }
         public string ImageLink { get; set; }
